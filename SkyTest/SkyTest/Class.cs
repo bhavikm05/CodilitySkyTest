d82a321a941532430516a29e4190c859ef696599
Rui Rosa Mendes <ruirosamendes@gmail.com>
2015-07-26 22:19:46 +0100

    Compiled Sky  test solution

---
+++ SkyTest/SkyTest/Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SkyTest
+{
+    class Class
+    {
+        public Class()
+        {
+        }
+
+        public void Function()
+        {
+        }
+
+
+    }
+
+    
+}
