376dd8278e781ae4b9bb546a2e5dd9ca31b8a38a
Rui Rosa Mendes <ruirosamendes@gmail.com>
2015-07-27 11:01:10 +0100

    Four tasks classes

--- SkyTest/SkyTest/Task2.cs
+++ SkyTest/SkyTest/Task2.cs
@@ -1,6 +1,6 @@
 ﻿using NUnit.Framework;
 
-namespace SkyTest
+namespace Task1
 {
     class Solution
     {        
---
+++ SkyTest/SkyTest/Task4.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace Task4
+{
+    class Solution
+    {        
+        public int solution(int[] A)
+        {
+            
+            return -1;
+        }
+    }
+
+    [TestFixture()]
+    public class SolutionTests
+    {
+       
+        [Test()]
+        public void Test1()
+        {
+            Solution instance = new Solution();
+            int[] A = new int[8];
+            A[0] = -1;
+            A[1] = 3;
+            A[2] = -4;
+            A[3] = 5;
+            A[4] = 1;
+            A[5] = -6;
+            A[6] = 2;
+            A[7] = 1;
+            int P = instance.solution(A);
+            Assert.AreEqual(P, 1);
+            //int P = instance.solution(A);
+            //Assert.AreEqual(P, 3);
+            //int P = instance.solution(A);
+            //Assert.AreEqual(P, 7);
+            //int P = instance.solution(A);
+            //Assert.AreEqual(P, -1);
+        }
+
+       
+
+    }
+
+
+}
